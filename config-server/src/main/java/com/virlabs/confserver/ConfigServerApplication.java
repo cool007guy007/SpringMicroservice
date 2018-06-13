360a70179a43d990ea5ebcffcb916bfb11ddc0d2
cool007guy007 <cool007.guy007@gmail.com>
2018-06-13 17:11:37 +0530

    2nd Commit for application.properties

--- config-server/src/main/java/com/virlabs/confserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/virlabs/confserver/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.virlabs.confserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
