6328242c0fe4a2499409827f7b9664afa3c33678
cool007guy007 <cool007.guy007@gmail.com>
2018-06-16 10:42:09 +0530

    Cloud config server working...

--- config-server/src/main/java/com/virlabs/confserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/virlabs/confserver/ConfigServerApplication.java
@@ -1,6 +1,7 @@
 package com.virlabs.confserver;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
