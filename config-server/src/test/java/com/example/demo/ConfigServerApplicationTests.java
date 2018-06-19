8bf6eea2bed55ed42d914bb90369f6c5664c3efb
cool007guy007 <cool007.guy007@gmail.com>
2018-06-19 16:24:56 +0530

    Eureka Service Registry

--- config-server/src/test/java/com/example/demo/ConfigServerApplicationTests.java
+++ config-server/src/test/java/com/example/demo/ConfigServerApplicationTests.java
@@ -1,12 +1,16 @@
 package com.example.demo;
 
+
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@SpringBootConfiguration
 public class ConfigServerApplicationTests {
 
 	@Test
