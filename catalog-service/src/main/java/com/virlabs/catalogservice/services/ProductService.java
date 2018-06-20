d3eba4ac8509fbe00ec89ac69f37f4062cc30cec
cool007guy007 <cool007.guy007@gmail.com>
2018-06-20 16:08:49 +0530

    Inventory RestTemplate

--- catalog-service/src/main/java/com/virlabs/catalogservice/services/ProductService.java
+++ catalog-service/src/main/java/com/virlabs/catalogservice/services/ProductService.java
@@ -1,15 +1,22 @@
 package com.virlabs.catalogservice.services;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 
 import com.virlabs.catalogservice.entities.Product;
 import com.virlabs.catalogservice.repositories.ProductRepository;
 
+import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
@@ -20,11 +27,12 @@ import lombok.extern.slf4j.Slf4j;
 public class ProductService {
 	
 	private final ProductRepository productRepository;
+	private final RestTemplate restTemplate;
 	
 	@Autowired
-	public ProductService(ProductRepository productRepository){
-		
+	public ProductService(ProductRepository productRepository,RestTemplate restTemplate){
 		this.productRepository = productRepository;
+		this.restTemplate= restTemplate;
 	}
 	
 	public List<Product>  findAllProducts(){
@@ -32,7 +40,23 @@ public class ProductService {
 	}
 	
 	public Optional<Product> findByProductCode(String productCode) {
-		return productRepository.findByCode(productCode);
+		 Optional<Product> productoptional = productRepository.findByCode(productCode);
+		 System.out.println("Fetching inventory level for product_code: "+productCode);
+		 
+		 ResponseEntity<ProductInventoryResponse> itemResponse = restTemplate.getForEntity("http://inventory-service/api/inventory/{productCode}", ProductInventoryResponse.class, productCode);
+		 if(itemResponse.getStatusCode()==HttpStatus.OK) {
+			 
+			Integer quantity = itemResponse.getBody().getAvailableQuantity();
+			System.out.println("Available Quantity"+quantity);
+			
+			productoptional.get().setInStock(quantity>0);
+		 }else {
+                System.out.println("Unable to get inventory level for product_code: "+productCode +
+                ", StatusCode: "+itemResponse.getStatusCode());
+            }
+
+		 
+		 return productoptional;
 	}
 
 }
---
+++ inventory-service/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+DELETE FROM inventory;
+
+insert into inventory(id, product_code, quantity) VALUES
+(1, 'P001', 100),
+(2, 'P002', 200),
+(3, 'P003', 300)
+;
